3e19a07c0ecc3548679a4a396c227215a3467b7f
Jasim Uddin <jasim.ice.iu@gmail.com>
2024-03-02 16:36:06 +0100

    form validation added for error

--- src/components/MuiTextField.tsx
+++ src/components/MuiTextField.tsx
@@ -1,22 +1,64 @@
-import { Stack, TextField } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import { InputAdornment, Stack, TextField } from "@mui/material";
+import { useState } from "react";
 
 export default function MuiTextField() {
+  const [value, setValue] = useState("");
+  return (
+    <Stack spacing={4}>
+      <Stack direction="row" spacing={2}>
+        <TextField label="Name" variant="standard" />
+        <TextField label="Name" variant="outlined" />
+        <TextField label="Name" variant="filled" />
+      </Stack>
 
-    return(
-        <Stack spacing={4}>
-            <Stack direction='row' spacing={2}>
-              <TextField label='Name' variant="standard"/>
-              <TextField label="Name" variant="outlined"/>
-              <TextField label="Name" variant="filled"/>
-            </Stack>
-            <Stack spacing={4} direction="row">
-                <TextField label="Small secondary" size="small" color="secondary"/>
-            </Stack>
-            <Stack spacing={4} direction="row">
-                <TextField label="Form Input" required/>
-                <TextField label="Password" type="password" helperText="Do not share password" disabled/>
-                <TextField label="Read Only"  inputProps={{readOnly:true}}/>
-            </Stack>
-        </Stack>
-    )
+      <Stack spacing={4} direction="row">
+        <TextField label="Small secondary" size="small" color="secondary" />
+      </Stack>
+
+      <Stack spacing={4} direction="row">
+        <TextField
+          label="Form Input"
+          required
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          error={!value} //if value is empty, show error
+          helperText={!value ? "Please enter a value" : ""} //if value is empty, show error
+        />
+        <TextField
+          label="Password"
+          type="password"
+          helperText="Do not share password"
+          disabled
+        />
+        <TextField label="Read Only" inputProps={{ readOnly: true }} />
+      </Stack>
+
+      <Stack direction="row" spacing={2}>
+        <TextField
+          label="Amount"
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="Weight"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+          }}
+        />
+
+        <TextField
+          label="TextField"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+    </Stack>
+  );
 }
